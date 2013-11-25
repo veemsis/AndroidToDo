78589d51cc56d3d68f62b3fe895e3a15c8af60ad
Karol <c.cisowski+code@gmail.com>
2013-11-25 23:15:12 +0100

    add button in action bar

--- gen/com/project/androidtodo/R.java
+++ gen/com/project/androidtodo/R.java
@@ -21,10 +21,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_add_to_queue=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
+        public static final int add_note=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,6 +36,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_note=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
     }
